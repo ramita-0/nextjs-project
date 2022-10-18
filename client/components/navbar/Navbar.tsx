49c03c607440418f1a09e9f290f5a925d6dc2085
ramita-0 <ramiromoya321@gmail.com>
2022-10-18 16:37:20 -0300

    tsx agregado

---
+++ client/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
